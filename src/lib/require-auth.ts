d6157341b48389e6a79368830050b1cc7a2f4415
shariski <falahudin.shariski@aignusahub.com>
2026-04-18 19:40:49 +0700

    fix(auth): remove dead eslint-disable comment in requireAuth

--- src/lib/require-auth.ts
+++ src/lib/require-auth.ts
@@ -5,7 +5,6 @@ import { auth } from "../server/auth"
 export async function requireAuth() {
   const request = getRequest()
   const session = await auth.api.getSession({ headers: request.headers })
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!session) throw redirect({ to: "/login" as any })
   return session
 }
